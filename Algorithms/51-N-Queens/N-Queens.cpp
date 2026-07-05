685ef69e0351a56baa063f2c2377c83b8415da6f
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-05 17:00:45 +0530

    Time: 3 ms (62.1%) | Memory: 10.2 MB (78.87%) - LeetCommit

---
+++ Algorithms/51-N-Queens/N-Queens.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    bool check(int row,int col , vector<string> &board , vector<vector<string>> &result , int n){
+        int temp_row = row;
+        int temp_col = col;
+        while(temp_col >= 0){
+            if(board[row][temp_col] == 'Q')return false;
+            temp_col--;
+        }
+        temp_col = col;
+        while(temp_row>=0 && temp_col>=0){
+            if(board[temp_row][temp_col] == 'Q')return false;
+            temp_row--;
+            temp_col--;
+        }
+        temp_row = row;
+        temp_col = col;
+        while(temp_row< n && temp_col>=0){
+            if(board[temp_row][temp_col] == 'Q')return false;
+            temp_row++;
+            temp_col--;
+        }
+        return true;
+    }
+
+    void create(int col, vector<string> &board , vector<vector<string>> &result,int n){
+        if(col==n){
+            result.push_back(board);
+            return;
+        }
+        for(int i = 0 ; i < n ;i++){
+            if(check(i,col,board,result,n)){
+                board[i][col] = 'Q';
+                create(col+1,board,result,n);
+                board[i][col] = '.';
+            }
+        }
+
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>result;
+        vector<string> board(n, string(n,'.'));
+        create(0,board,result,n);
+        return result;
+    }
+};
