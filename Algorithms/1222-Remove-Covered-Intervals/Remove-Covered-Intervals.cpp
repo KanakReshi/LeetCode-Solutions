00961cc81dc261d672c3ed996b5d28e4d532cc0b
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-06 14:05:31 +0530

    Time: 20 ms (11.92%) | Memory: 15.5 MB (19.47%) - LeetCommit

--- Algorithms/1222-Remove-Covered-Intervals/Remove-Covered-Intervals.cpp
+++ Algorithms/1222-Remove-Covered-Intervals/Remove-Covered-Intervals.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        // sort(intervals.begin() , intervals.end());
-        int len = intervals.size();
-        vector<bool>check(len,true);
-        for(int i = 0 ; i < len ; i++){
-            for(int j = 0 ; j < len;j++){
-                if(i==j)continue;
-                if(intervals[j][0] >= intervals[i][0] && intervals[j][1] <= intervals[i][1]){
-                    check[j] = false;
-                }
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b) {
+            if (a[0] == b[0])
+                return a[1] > b[1];
+            return a[0] < b[0];
+        });
+        int count = 0;
+        vector<bool> mark(intervals.size(), false);
+        for(int i = 0; i < intervals.size() - 1; i++) {
+            for(int j = i + 1; j < intervals.size(); j++) {
+                if(intervals[i][0] <= intervals[j][0] &&
+                    intervals[i][1] >= intervals[j][1] &&
+                    !mark[j]) {
+                        count++;
+                        mark[j] = true;
+                    }
             }
         }
-        int result = 0;
-        for(auto it : check){
-            if(it)result++;
-        }
-        return result;
+        return intervals.size() - count;
     }
 };
