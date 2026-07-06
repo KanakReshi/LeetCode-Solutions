1aa7ff3f380c0b2c4ea882f1403947f1a170d63a
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-06 14:00:27 +0530

    Time: 63 ms (8.87%) | Memory: 15.5 MB (19.47%) - LeetCommit

---
+++ Algorithms/1222-Remove-Covered-Intervals/Remove-Covered-Intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin() , intervals.end());
+        int len = intervals.size();
+        vector<bool>check(len,true);
+        for(int i = 0 ; i < len ; i++){
+            for(int j = 0 ; j < len;j++){
+                if(i==j)continue;
+                if(intervals[j][0] >= intervals[i][0] && intervals[j][1] <= intervals[i][1]){
+                    check[j] = false;
+                }
+            }
+        }
+        int result = 0;
+        for(auto it : check){
+            if(it)result++;
+        }
+        return result;
+    }
+};
