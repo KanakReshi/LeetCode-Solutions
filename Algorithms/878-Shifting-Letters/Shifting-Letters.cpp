5a3beb830de7c635d18dada54753af55e6b1f727
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-08 23:18:57 +0530

    Time: 8 ms (29.59%) | Memory: 77 MB (26.83%) - LeetCommit

---
+++ Algorithms/878-Shifting-Letters/Shifting-Letters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = s.size();
+        vector<long long> suffix(n);
+        suffix[n - 1] = shifts[n - 1] % 26;
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = (suffix[i + 1] + shifts[i]) % 26;
+        }
+        for (int i = 0; i < n; i++) {
+            s[i] = 'a' + (s[i] - 'a' + suffix[i]) % 26;
+        }
+        return s;
+    }
+};
