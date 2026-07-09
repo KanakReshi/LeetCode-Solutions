95e707ee161736a384c6d317c1a1342379ce2dba
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-09 16:42:06 +0530

    Time: 4 ms (72.67%) | Memory: 98.9 MB (35.15%) - LeetCommit

---
+++ Algorithms/2465-Shifting-Letters-II/Shifting-Letters-II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        vector<long long> diff(n+1,0);
+        for(int i = 0 ; i < shifts.size() ; i++){
+            int start = shifts[i][0];
+            int end = shifts[i][1];
+            int direction = shifts[i][2]==1?1:-1;
+            diff[start] += direction;
+            diff[end + 1] -= direction;
+        }
+        int steps = 0;
+        for(int i = 0 ; i < n ; i++){
+            steps += diff[i];
+            int shift = ((steps % 26) + 26) % 26;
+            s[i] = 'a' + (s[i] - 'a' + shift) % 26;
+        }
+        return s;
+    }
+};
