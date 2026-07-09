7f18239f2a496145063496d854a71c7ea8437550
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-09 23:27:24 +0530

    Time: 67 ms (16.63%) | Memory: 23 MB (16.06%) - LeetCommit

---
+++ Algorithms/386-Lexicographical-Numbers/Lexicographical-Numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<string>lexo(n+1);
+        lexo[0] = "00";
+        for(int i = 1 ;i <= n;i++){
+            lexo[i] = to_string(i);
+        }
+        sort(lexo.begin() , lexo.end());
+        vector<int>result;
+        for(int i = 1;i <= n;i++){
+            result.push_back(stoi(lexo[i]));
+        }
+        return result;
+    }
+};
