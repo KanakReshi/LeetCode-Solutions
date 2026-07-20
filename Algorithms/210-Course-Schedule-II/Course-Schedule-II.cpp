5701516a606e7185ecbd74c9c2438bf6d04f81e8
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-20 22:50:04 +0530

    Time: 3 ms (70.94%) | Memory: 18.3 MB (71.13%) - LeetCommit

---
+++ Algorithms/210-Course-Schedule-II/Course-Schedule-II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<int> result;
+        vector<int> parent(n, 0);
+        vector<vector<int>> adj(n);
+
+        for (int i = 0; i < prerequisites.size(); i++) {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            parent[prerequisites[i][0]]++;
+        }
+        int j = 0;
+        // int k = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == 0)
+                result.push_back(i);
+        }
+        while (j < result.size()) {
+            int node = result[j];
+            j++;
+            // k++;
+            for (int it : adj[node]) {
+                parent[it]--;
+                if (parent[it] == 0)
+                    result.push_back(it);
+            }
+        }
+        if (j == n)
+            return result;
+        else
+            return {};
+    }
+};
