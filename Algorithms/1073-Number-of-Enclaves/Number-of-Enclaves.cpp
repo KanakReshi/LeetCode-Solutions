0709303d2051c74732ebf1ddfc8d8288416bcf03
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-22 17:08:07 +0530

    Time: 181 ms (6.47%) | Memory: 51.5 MB (7.36%) - LeetCommit

---
+++ Algorithms/1073-Number-of-Enclaves/Number-of-Enclaves.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& grid,int i , int j){
+        if(grid[i][j] == 1)grid[i][j] = '#';
+        else return;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int>dir = {0,1,0,-1,0};
+        for(int k = 0 ; k  < 4 ; k++){
+            int nr = i + dir[k];
+            int nc = j + dir[k+1];
+            if(nc < 0 || nr < 0 || nc >= n || nr >=m || grid[nr][nc] == '#')continue;
+            dfs(grid,nr,nc);
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i = 0 ; i < m ;i++){
+            if(grid[i][0] == 1)dfs(grid,i,0);
+            if(grid[i][n-1] == 1)dfs(grid,i,n-1);
+        }
+        for(int i = 0 ; i < n ;i++){
+            if(grid[0][i] == 1)dfs(grid,0,i);
+            if(grid[m-1][i] == 1)dfs(grid,m-1,i);
+        }
+        int result = 0;
+        for(int i = 0 ; i < m ;i++){
+            for(int j = 0 ; j < n;j++){
+                if(grid[i][j] == 1)result++;
+            }
+        }
+        return result;
+    }
+};
