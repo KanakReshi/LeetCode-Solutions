d57ff63a25746f69ea0aa8774175573912d66733
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-07-24 00:47:55 +0530

    Time: 47 ms (90.17%) | Memory: 75.9 MB (45.18%) - LeetCommit

---
+++ Algorithms/516-Longest-Palindromic-Subsequence/Longest-Palindromic-Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int create(string &s , int i , int j,vector<vector<int>> &dp){
+        if(i > j)return 0;
+        if(i==j)return 1;
+        if(dp[i][j] != -1)return dp[i][j];
+        if(s[i] == s[j])return 2 + create(s,i+1,j-1,dp);
+        int left_side = create(s,i,j-1,dp);
+        int right_side = create(s,i+1,j,dp);
+        return dp[i][j] = max(left_side,right_side);
+    }
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return create(s,0,n-1,dp);
+    }
+};
