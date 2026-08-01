e42e40d912cd93da00cf0c4a285890bf74544be9
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-08-01 22:52:33 +0530

    Time: 0 ms (100%) | Memory: 17.6 MB (99.5%) - LeetCommit

--- Algorithms/152-Maximum-Product-Subarray/Maximum-Product-Subarray.cpp
+++ Algorithms/152-Maximum-Product-Subarray/Maximum-Product-Subarray.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int right = 1;
-        int left = 1;
+        int n = nums.size();
+        int maxelem = 1;
+        int minelem = 1;
         int result = INT_MIN;
-        int len = nums.size();
-        for(int i = 0; i<len;i++){
-            if(left==0)left=1;
-            if(right==0)right=1;
-            right = right*nums[i]; 
-            left = left * nums[len-i-1];
-            result = max(result,max(left,right));
+        for(int i = 0 ; i< n ;i++){
+            int v1 = maxelem*nums[i];
+            int v2 = nums[i];
+            int v3 = minelem*nums[i];
+            maxelem = max(v1,max(v2,v3));
+            minelem = min(v1,min(v2,v3));
+            result = max(result,maxelem);
         }
-
         return result;
     }
 };
