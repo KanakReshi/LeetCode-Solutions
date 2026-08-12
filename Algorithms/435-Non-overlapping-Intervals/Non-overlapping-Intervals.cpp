c8aeaef8858f87b4cd2e4df78e59483655a6700f
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-08-13 00:55:53 +0530

    Time: 30 ms (98.84%) | Memory: 94 MB (69.11%) - LeetCommit

--- Algorithms/435-Non-overlapping-Intervals/Non-overlapping-Intervals.cpp
+++ Algorithms/435-Non-overlapping-Intervals/Non-overlapping-Intervals.cpp
@@ -5,17 +5,17 @@ public:
         sort(intervals.begin(),intervals.end(),[](vector<int>&a,vector<int>&b){
             return a[1]<b[1];
         });
-        // int start1 = intervals[0][0];
+        int start1 = intervals[0][0];
         int end1 = intervals[0][1];
         int remove = 0;
         for(int i = 1 ;i < n;i++){
             int start2 = intervals[i][0];
-            // int end2 = intervals[i][1];
+            int end2 = intervals[i][1];
             if(start2 < end1){
                 remove++;
             }
             else{
-                // start1 = intervals[i][0];
+                start1 = intervals[i][0];
                 end1 = intervals[i][1];
             }
         }
