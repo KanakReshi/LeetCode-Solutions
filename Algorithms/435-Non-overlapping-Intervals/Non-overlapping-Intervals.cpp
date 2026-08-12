beb790dede246e1e44f303c45cdd29830be0bb60
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-08-13 00:55:08 +0530

    Time: 40 ms (78.43%) | Memory: 93.9 MB (91.35%) - LeetCommit

---
+++ Algorithms/435-Non-overlapping-Intervals/Non-overlapping-Intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end(),[](vector<int>&a,vector<int>&b){
+            return a[1]<b[1];
+        });
+        // int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+        int remove = 0;
+        for(int i = 1 ;i < n;i++){
+            int start2 = intervals[i][0];
+            // int end2 = intervals[i][1];
+            if(start2 < end1){
+                remove++;
+            }
+            else{
+                // start1 = intervals[i][0];
+                end1 = intervals[i][1];
+            }
+        }
+        return remove;
+    }
+};
