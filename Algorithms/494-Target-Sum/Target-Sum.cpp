640bb40ae3105e0fbd10fa7fbd88668fd88d264e
Kanak Reshi <126339409+KanakReshi@users.noreply.github.com>
2026-08-15 15:36:08 +0530

    Time: 692 ms (19.54%) | Memory: 13.8 MB (61.04%) - LeetCommit

--- Algorithms/494-Target-Sum/Target-Sum.cpp
+++ Algorithms/494-Target-Sum/Target-Sum.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int create(vector<int>& nums, int sum, int target, int i, int n,
-               vector<vector<int>>& dp,int arr_sum) {
+               vector<vector<int>>& dp) {
         if (i == n) {
             return (sum == target) ? 1 : 0;
         }
-        if(dp[i][arr_sum + sum] != -1)return dp[i][arr_sum + sum]; 
         int elem = nums[i];
-        int positive = create(nums, sum - elem, target, i + 1, n, dp,arr_sum);
-        int negative = create(nums, sum + elem, target, i + 1, n, dp,arr_sum);
-        return dp[i][arr_sum + sum] = positive + negative;
+        int positive = create(nums, sum - elem, target, i + 1, n, dp);
+        int negative = create(nums, sum + elem, target, i + 1, n, dp);
+        return positive + negative;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int arr_sum = 0;
-        for(int it : nums){
-            arr_sum += it;
-        }
-        vector<vector<int>> dp(n, vector<int>(2*(arr_sum) + 1, -1));
-        return create(nums, 0, target, 0, n, dp, arr_sum);
+        vector<vector<int>> dp(n, vector<int>(abs(target) + 1, -1));
+        return create(nums, target, 0, 0, n, dp);
     }
 };
